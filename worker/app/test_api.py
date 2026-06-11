f2c83b4dccf01959e1471fc62e5c815b33da15e0
Stefan Heinecke <stefan.heinecke1@gmail.com>
2026-06-11 21:59:45 +0200

    fix

--- worker/app/test_api.py
+++
@@ -1,4 +0,0 @@
-import requests
-r = requests.get("https://etfdata-production.up.railway.app/etfs", params={"limit":10},
-    headers={"x-api-key":"_jlTM8d5UQbi0gmgpNqTARo8ObzUPXzmMm-s241o49SitxZgrPa7TMsh0KvJk6Jt"})
-print(r.json())
